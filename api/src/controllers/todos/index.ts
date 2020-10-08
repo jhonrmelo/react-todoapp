1ed9fe64dfd54d9f54fc49fe795258e9e2417b20
jhonrmelo <45439356+jhonrmelo@users.noreply.github.com>
2020-10-07 22:36:33 -0300

    Working with api and endpoints

---
+++ api/src/controllers/todos/index.ts
@@ -0,0 +1,79 @@
+import { Response, Request } from "express"
+import { ITodo } from "./../../types/todo"
+import Todo from "../../models/todo"
+
+//Todo: Refatorar isso igual é feito na aplicação da rocket
+
+const getTodos = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const todos: ITodo[] = await Todo.find()
+        res.status(200).json({ todos })
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+const addTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const body = req.body as Pick<ITodo, "name" | "description" | "status">
+
+        const todo: ITodo = new Todo({
+            name: body.name,
+            description: body.description,
+            status: body.status
+        })
+        const newTodo: ITodo = await todo.save()
+        const allTodos: ITodo[] = await Todo.find()
+
+        res.status(201).json({ message: "Todo Added", todo: newTodo, todos: allTodos })
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+
+const updateTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {
+            params: { id },
+            body,
+        } = req
+        const updateTodo: ITodo | null = await Todo.findByIdAndUpdate(
+            { _id: id },
+            body
+        )
+        const allTodos: ITodo[] = await Todo.find()
+        res.status(200).json({
+            message: "Todo Updated",
+            todo: updateTodo,
+            todos: allTodos
+        })
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+const deleteTodo = async (req: Request, res: Response): Promise<void> => {
+
+    try {
+        const deletedTodo: ITodo | null = await Todo.findByIdAndRemove(
+            req.params.id
+        )
+        const allTodos: ITodo[] = await Todo.find()
+
+        res.status(200).json({
+            message: "Todo Deleted",
+            todo: deleteTodo,
+            todos: allTodos
+        })
+
+    }
+    catch (e) {
+        throw e
+    }
+}
+
+export { getTodos, addTodo, updateTodo, deleteTodo }
